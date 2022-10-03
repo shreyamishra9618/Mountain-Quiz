184928708068f1feb0c1da8b44779b6eb775a1bb
Shreya Mishra <shreya@Shreyas-MacBook-Pro.local>
2022-10-03 08:55:37 -0700

    Updated Java script pages

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var times=document.getElementById("timer")
+var instructions = document.querySelector("#instructions")
+var startGameButton = document.querySelector("#start-game")
+var game = document.querySelector("#game")
+var scoreInput = document.querySelector("scores-input")
+var correctAns= document.querySelector("#correct-ans")
+var questions=document.getElementById("questions")
+var ansButtons=document.getElementById("ans-buttons")
+var score=document.getElementById("score")
+
+// Create three variables for time storage, shuffling and counter
+var timeLeft;
+var shuffledQuestions;
+var counter;
+
+// funtion for shuffling the question using math.random
+
+// funtion to start the game
+
+//funtion to load next question
+// funtion for correct and
+// funtion for wrong answer
+// funtion for end game
+// add event listner on start quiz
+// add event listner on submit of quiz
