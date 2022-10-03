ba9de87e863da8f9754d9bfdf34314f060cc9ec0
Shreya Mishra <shreya@Shreyas-MacBook-Pro.local>
2022-10-02 21:38:12 -0700

    created all folders and html basic structure

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        question:"In which of the following continent the Andes Mountain Range is located?",
+        answers:['Australia',"Europe","S. America","N. America"],
+        correctAnswer: "S. America"
+    },
+    {
+        question:"The largest mountain series of the world?",
+        answers:['Himalaya',"Andes","Rockies","Alps"],
+        correctAnswer: "Andes"
+    },
+    {
+        question:"Highest peaks of the world are mostly found in which type of mountains?",
+        answers:["Old folded mountains","Young fold mountains", "Residual mountains", "Block mountains"],
+        correctAnswer: "Young fold mountains"
+    },
+    {
+        question:"Which of the following is known as the 'Sea of mountains'?",
+        answers:["Japan","British Columbia", "West coast of South America", "South East Asia"],
+        correctAnswer: "British Columbia"
+    },
+    {
+        question:"Which of the following is the longest mountain range?",
+        answers:["The Rocky","The Alps", "The Himalayas", "The Andes"],
+        correctAnswer: "The Andes"
+    },
+    {
+        question:"White Mountains are founded in?",
+        answers:["Canada","Norway", "Russia", "USA"],
+        correctAnswer: "USA"
+    },
+    {
+        question:"Which of the following mountains is located in Germany?",
+        answers:["Black Forest","Atlas", "Pyrenees", "Apennines"],
+        correctAnswer: "Black Forest"
+    },
+    {
+        question:"Black Mountain is situated in?",
+        answers:["Canada","Norway", "Switzerland", "USA"],
+        correctAnswer: "USA"
+    },
+    {
+        question:"Drakensberg is a mountain of?",
+        answers:["Botswana","Namibia", "South Africa", "Zambia"],
+        correctAnswer: "South Africa"
+    },
+    {
+        question:"Which of the following mountains is located in Italy?",
+        answers:["Apennine","Pyrenees", "Dimaric Alps", "Zura"],
+        correctAnswer: "Apennine"
+    },
+]
